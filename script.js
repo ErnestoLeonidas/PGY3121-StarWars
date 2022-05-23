ba4908b77ce5ee2155311aca326bc43e93540824
ernestoleonidas <ernestoleonidas@gmail.com>
2022-05-22 20:41:54 -0400

    Login 2.0

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
+    var email;
+    var pass;
+    
+    $("#btn_acceder").click(function(){
+        email = $("#email").val();
+        pass = $("#password").val();
+        //console.log(email + ' '+ pass);
+        validar_acceso_v2(email, pass);
+    });
 
-        var email;
-        var pass;
-        
-        $("#btn_acceder").click(function(){
-            email = $("#email").val();
-            pass = $("#password").val();
-            //console.log(email + ' '+ pass);
-            validar_acceso_v2(email, pass);
-        });
-
-        function validar_acceso_v1(_email, _pass){
-            if (_email == 'ernestoleonidas@gmail.com' && _pass == '123') {
-                console.log('SI');
-            } else {
-                console.log('NO');
-            }
+    function validar_acceso_v1(_email, _pass){
+        if (_email == 'ernestoleonidas@gmail.com' && _pass == '123') {
+            console.log('SI');
+        } else {
+            console.log('NO');
         }
+    }
 
-        validar_acceso_v2 = (_email, _pass) => {
-            if (_email == 'ernestoleonidas@gmail.com' && _pass == '123') {
-                console.log('SI');
-            } else {
-                console.log('NO');
-            }
+    validar_acceso_v2 = (_email, _pass) => {
+        if (_email == 'ernestoleonidas@gmail.com' && _pass == '123') {
+            console.log('SI');
+            $(location).attr('href','dashboard.html');
+        } else {
+            console.log('NO');
         }
+    }
 
-        $("#btn_limpiar").click(function(){
-            $("#email").val('');
-            $("#password").val('');
-        });
+    $("#btn_limpiar").click(function(){
+        $("#email").val('');
+        $("#password").val('');
+    });
